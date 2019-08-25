fa63d60dfe5817d4e25b78445efcc2fba97ae651
Rayferrufino <52383730+Rayferrufino@users.noreply.github.com>
2019-08-25 14:32:13 -0400

    Update and rename 4.SLMail-Pattern_create.md to 4.SLMail-Pattern_create.bash

---
+++ 4.SLMail-Pattern_create.bash
@@ -0,0 +1,5 @@
+root@kali:~# locate pattern_create
+/usr/share/metasploit-framework/tools/exploit/pattern_create.rb
+
+root@kali:~# /usr/share/metasploit-framework/tools/exploit/pattern_create.rb -l 2700
+Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3A
